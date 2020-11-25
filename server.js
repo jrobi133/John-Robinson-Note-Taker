916a9ff240b9fc06d5a82dab2c78ccc40814d584
John Robinson <johnarobinson133@gmail.com>
2020-11-25 11:50:06 -0700

    added api routes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var fs = require("fs");
 var express = require("express");
 var app = express();
 var tableData = require(".../db/db.json");
-let notes = [];
+
 // port
 let PORT = process.env.PORT || 3001
 
@@ -12,35 +12,53 @@ app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "notes.html"));
   });
   
-  app.get("*", function(req, res) {
+app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
-  });
+});
   
   // api routes
-  app.get("/api/db", function(req, res) {
-    res.json(tableData);
-  });
-  
-  app.post("/api/db", function(req, res) {
-  
-    if (tableData.length < 5) {
-      tableData.push(req.body);
-      res.json(true);
-    }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
-  });
-  
-  app.delete("/api/notes/:id", (req, res) => {
-    fs.readFile("db/db.json", "utf-8", (err, data) => {
+app.get("/api/notes", function(req, res) {
+  fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+    try {
       let db = JSON.parse(data);
-      db.splice(req.params.id, 1);
-      let reindex_db = db.map((currentElement, index) => {
-        currentElement = { ...currentElement, id: index };
-        return currentElement;
-      });
+      res.send(db);
+    } catch (e) {
+      let arr = [];
+      let obj = JSON.parse('{' + data + '}');
+      arr.push(obj);
+      res.send(arr);
+    }
+  });
+});
+  
+app.post("/api/notes", function(req, res) {
+  let { body } = req;
+  if (body === undefined) {
+    res.send(`Undefined!`);
+    return;
+  }
+  fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+    let db = JSON.parse(data);
+    body.id = db.length;
+    db.push(body);
+    fs.writeFile('./db/db.json', JSON.stringify(db), err => {
+      if(!err) {
+        res.send('ok');
+      } else {
+        throw err;
+      }
+    });
+  });
+});
+  
+app.delete("/api/notes/:id", (req, res) => {
+  fs.readFile("db/db.json", "utf-8", (err, data) => {
+    let db = JSON.parse(data);
+    db.splice(req.params.id, 1);
+    let reindex_db = db.map((currentElement, index) => {
+      currentElement = { ...currentElement, id: index };
+      return currentElement;
+    });
       fs.writeFile("./db/db.json", JSON.stringify(reindex_db), (err) => {
         if (!err) {
           res.send("ok");
@@ -54,6 +72,6 @@ app.get("/notes", function(req, res) {
 
 // Server listening
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log("App listening on PORT" + PORT);
   });
   
